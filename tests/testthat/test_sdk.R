0486fcf5d7662484b90f97b7c59831fb5e9a4565
Ken McHugh <kmchugh@icatalyst.com>
2020-08-30 23:17:04 +0800

    Added shiny support

--- tests/testthat/test_sdk.R
+++ tests/testthat/test_sdk.R
@@ -4,7 +4,7 @@ library(com.icatalyst.singularity)
 test_that('the constructor requires client_id and client_secret', {
   client_id <- 'a client id'
   client_secret <- 'a client secret'
-  sut = com.icatalyst.singularity::Singularity$new('a client id', 'a client secret')
+  sut = com.icatalyst.singularity::Singularity$new('a client id', 'a client secret', 'a client key')
   expect_true(R6::is.R6(sut))
 })
 
@@ -12,11 +12,40 @@ test_that('the constructor requires client_id and client_secret', {
 test_that('the default configuration loads correctly', {
   client_id <- 'a client id'
   client_secret <- 'a client secret'
-  sut = com.icatalyst.singularity::Singularity$new('a client id', 'a client secret')
+  sut = com.icatalyst.singularity::Singularity$new('a client id', 'a client secret', 'a client key')
   expect_equal(sut$server_uri, 'https://api.singularity.icatalyst.com')
 
   expect_equal(sut$endpoints[['token']], '/token')
+})
 
+test_that('it can create a javascript message handler for shiny', {
+  client_id <- 'a client id'
+  client_secret <- 'a client secret'
+  sut = com.icatalyst.singularity::Singularity$new('a client id', 'a client secret', 'a client key')
 
+  expect_equal(
+    'Shiny.addCustomMessageHandler(\'singularity_redirect\', function(message) {window.location = \'https://api.singularity.icatalyst.com\';});',
+    sut$shiny_redirect_code)
+
+  sut = com.icatalyst.singularity::Singularity$new('a client id', 'a client secret', 'a client key', config=list(
+    server_uri = 'https://test.test.com'
+  ))
+
+  expect_equal(
+    'Shiny.addCustomMessageHandler(\'singularity_redirect\', function(message) {window.location = \'https://test.test.com\';});',
+    sut$shiny_redirect_code)
+
+  sut = com.icatalyst.singularity::Singularity$new('a client id', 'a client secret', 'a client key')
+  sut$shiny_tags(
+    tags = list(
+      head = function(a){},
+      script = function(a){}
+    ),
+    redirect_uri = 'https://www.testing.test.com'
+  )
+
+  expect_equal(
+    'Shiny.addCustomMessageHandler(\'singularity_redirect\', function(message) {window.location = \'https://www.testing.test.com\';});',
+    sut$shiny_redirect_code)
 })
 
